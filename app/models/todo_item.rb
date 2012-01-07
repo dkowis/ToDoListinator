7b588619714ce8168f316141fbe3460e7b82d2a7
David Kowis <dkowis@shlrm.org>
2012-01-07 17:12:59 -0600

    Improved my model and removed tests that I don't use

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,4 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
-  validates_presence_of :entry
+  validates_presence_of :entry, :todo_list
 end
--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -1,16 +1,31 @@
 require 'spec_helper'
 
 describe TodoItem do
+
+  before do
+    @valid_item = TodoItem.new
+    @valid_item.todo_list = TodoList.new(:title => "blarg")
+    @valid_item.entry = "blarg"
+  end
+
   it "should belong to a TodoList" do
     association = TodoItem.reflect_on_association(:todo_list)
     association.macro.should == :belongs_to
   end
 
   it "requires an entry" do
-    item = TodoItem.new
-    item.should_not be_valid
+    @valid_item.entry = nil
+    @valid_item.should_not be_valid
 
-    item.entry = "blargag"
-    item.should be_valid
+    @valid_item.entry = "blargag"
+    @valid_item.should be_valid
+  end
+
+  it "requires a Todo List" do
+    @valid_item.todo_list = nil
+    @valid_item.should_not be_nil
+
+    @valid_item.todo_list = TodoList.new(:title => "blarg")
+    @valid_item.should be_valid
   end
 end
