4549adce4703bf87853b7001df6b1e2909d67ad7
David Kowis <dkowis@shlrm.org>
2012-01-07 23:30:23 -0600

    adding a validation to the date to check if it's in the past

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,12 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
   validates_presence_of :entry, :todo_list
+  validate :due_date_future, :if => Proc.new { |a| a.due_date }
+
+  private
+  def due_date_future
+    if due_date < Date.today
+      errors.add(:due_date, "cannot be in the past!")
+    end
+  end
 end
