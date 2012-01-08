d0182fe9f557b22b62a829a966e16589c3811df4
David Kowis <dkowis@shlrm.org>
2012-01-08 12:19:53 -0600

    there might be a better way to do the sorting, but I'm not sure
    sqlite doesn't support specifying the order of nulls

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,7 +1,22 @@
 class TodoItem < ActiveRecord::Base
+  includes Comparable
+
   belongs_to :todo_list
   validates_presence_of :entry, :todo_list
   validate :due_date_future, :on => :create, :if => Proc.new{ |a| a.due_date }
+
+  def <=>(other)
+    if other.due_date and due_date
+      due_date <=> other.due_date
+    elsif !other.due_date and due_date
+      -1
+    elsif !due_date and other.due_date
+      1
+    else
+      id <=> other.id
+    end
+  end
+
 
   private
   def due_date_future
--- spec/models/todo_item_spec.rb
+++ spec/models/todo_item_spec.rb
@@ -28,4 +28,39 @@ describe TodoItem do
     @valid_item.todo_list = TodoList.new(:title => "blarg")
     @valid_item.should be_valid
   end
+
+  describe "a list of todo_items" do
+    it "are sorted by due_date, with items without a due date at the end" do
+      items = Array.new
+
+      undated_items = Array.new
+      3.times do |x|
+        item = TodoItem.new(:entry => "Undated Entry #{x}")
+        item.id = x
+        items << item
+        undated_items << item
+      end
+
+      dated_items = Array.new
+      5.times do |x|
+        item = TodoItem.new(:entry => "Dated entry #{x}")
+        item.id = x+10
+        item.due_date = x.days.from_now
+        items << item
+        dated_items << item
+      end
+
+      items.sort!
+
+      items.size.times do |x|
+        if x <5
+          items[x].should == dated_items[x]
+        else
+          items[x].should == undated_items[x-5]
+        end
+      end
+
+
+    end
+  end
 end
