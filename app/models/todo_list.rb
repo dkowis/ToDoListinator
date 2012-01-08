f767092e392166fea474fbbe9059771f26106b73
David Kowis <dkowis@shlrm.org>
2012-01-07 22:03:03 -0600

    Sorting by two fields!

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,5 @@
 class TodoList < ActiveRecord::Base
-  has_many :todo_items
+  #NOTE: sqlite3 doesn't support nulls last, so I can't do cool things :(
+  has_many :todo_items, :order => "complete ASC, due_date DESC"
   validates_presence_of :title
 end
